658eef5e1b17234f5e6067fa2d6c0ee54eaac37d
Akpobari <augustinegolden994@gmail.com>
2023-03-20 21:55:27 -0700

    tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *main - Entry Point
- *
- *Return: Always 0 (Success)
- */
+*main - Entry Point
+*
+*Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-        int alpha;
+	int alpha;
 
-        alpha = 'z';
+	alpha = 'z';
 
-        while (alpha >= 'a')
-        {
-                putchar(alpha);
-                alpha -= 1;
-        }
-        putchar('\n');
-        return (0);
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha -= 1;
+	}
+	putchar('\n');
+	return (0);
 }
