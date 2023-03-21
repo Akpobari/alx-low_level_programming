98127e5e211f457f27985c80b78cdf34f21931aa
Akpobari <augustinegolden994@gmail.com>
2023-03-20 22:05:26 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 /**
- *main - Entry Point
- *
- *Return: Always 0 (Success)
- */
+*main - Entry Point
+*
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-        int n;
+	int n;
 
-        n = '0';
-        while (n <= '9')
-        {
-                putchar(n);
-                if (n != '9')
-                {
-                        putchar(',');
-                        putchar(' ');
-                }
-                ++n;
-        }
-        putchar('\n');
-        return (0);
+	n = '0';
+	while (n <= '9')
+	{
+		putchar(n);
+	if (n != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+		++n;
+	}
+	putchar('\n');
+	return (0);
 }
