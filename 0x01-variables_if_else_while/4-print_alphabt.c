7c5234ea3cfe93b6b2659b56649288d0913947cf
Akpobari <augustinegolden994@gmail.com>
2023-03-20 21:43:20 -0700

    alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- *main -Entry Point
- *
- *Return: Always 0 (Success)
- */
+*main -Entry Point
+*
+*Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-        int alpha;
-
-        alpha = 'a';
-        while (alpha <= 'z')
-        {
-                if (alpha == 'e' || alpha == 'q')
-                {
-                        alpha += 1;
-                }
-                else
-                {
-                        putchar(alpha);
-                        alpha += 1;
-                }
-        }
-        putchar('\n');
-        return (0);
+	int alpha;
+	
+	alpha = 'a';	
+	while (alpha <= 'z')
+	{
+	if (alpha == 'e' || alpha == 'q')
+	{
+		alpha += 1;
+	}
+	else
+	{
+		putchar(alpha);
+		alpha += 1;
+	}
+		putchar('\n');
+		return (0);
 }
