2370ad135e1b1d850b6ceffa47fe68e02350b3e0
Akpobari <augustinegolden994@gmail.com>
2023-03-21 22:38:03 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lower case
+ */
+
+void print_alphabet(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_pitchar(letter)
+
+	_putchar('\n');
+}
