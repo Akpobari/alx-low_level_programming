afc0f671d296777415c096078c7288400d3bdf41
Akpobari <augustinegolden994@gmail.com>
2023-03-24 11:09:06 -0700

    mule

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
-*
-*Return: void
-*/
-
-void print_numbers(void)
+* _isdigit - write a function that check for a digit (0 through 9).
+* @c: char to check
+* Return:  0 or 1
+**/
+int _isdigit(int c)
 {
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
