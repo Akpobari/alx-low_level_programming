88858c3486c35dd3df643d96d5dfb6b2126bb6b3
Akpobari <augustinegolden994@gmail.com>
2023-03-28 15:11:02 -0700

    print

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * printf - function that prints the integers as entity
  * print_array - function that prints n elements of an array of integers
  * followed by a new line
  * @a: this is the input array
